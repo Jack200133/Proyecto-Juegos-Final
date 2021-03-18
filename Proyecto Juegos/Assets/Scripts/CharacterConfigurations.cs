dd7fb614c4ab65a6bdd229437a03b2a14967b21f
Jack200133 <car20593@uvg.edu.gt>
2021-03-18 17:16:08 -0600

    Proyecto Final de Juegos terminado

---
+++ Proyecto Juegos/Assets/Scripts/CharacterConfigurations.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterConfigurations : MonoBehaviour
+{
+    public GameObject[] spawns;
+    public AudioSource [] bu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bu = GetComponents<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemie"))
+        {
+            bu[1].Play();
+            gameObject.transform.position = spawns[Random.Range(0, 3)].transform.position;
+
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Light")) {
+
+            gameObject.tag = "onLight";
+        
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Light"))
+        {
+
+            gameObject.tag = "notOnLight";
+        }
+    }
+}
